cd849646700184a0bf299cf089161fdb8de7b097
hamzafrigui <35801880+hamzafrigui@users.noreply.github.com>
2020-01-14 21:20:22 +0100

    Update ReservationController.java

--- Reservation-service/src/main/java/com/hotelreservation/Reservationservice/controller/ReservationController.java
+++ Reservation-service/src/main/java/com/hotelreservation/Reservationservice/controller/ReservationController.java
@@ -2,6 +2,7 @@ package com.hotelreservation.Reservationservice.controller;
 
 import com.hotelreservation.Reservationservice.model.Reservation;
 import com.hotelreservation.Reservationservice.repository.ReservationRepository;
+import com.hotelreservation.Reservationservice.service.ReservationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,6 +16,7 @@ public class ReservationController {
 
     @Autowired
     ReservationRepository reservationRepository;
+    ReservationService reservationService ;
 
     @GetMapping(value = "/reservations")
     public List<Reservation> getallReservation(){
@@ -25,11 +27,16 @@ public class ReservationController {
 
 
     @PostMapping(value = "/reservations")
-    public ResponseEntity<Reservation> addReservation(@RequestBody Reservation reservation
-    ){
-
-        Reservation newReservtion = reservationRepository.save(reservation);
-
+    public ResponseEntity<Reservation> addReservation(@RequestBody Reservation reservation){
+        List<Reservation> listeReserv=reservationRepository.findAll() ;
+        int i=0 ;
+        while (i< listeReserv.size()) {
+            if (reservation.getDateReservationfin().compareTo(reservationRepository.findAll().get(i).getDateReservationdebut())<0) {
+                Reservation newReservtion = reservationRepository.save(reservation);
+                break;
+            }
+            i++;
+        }
         //if(nouvelleReservtion == null) throw new ImpossibleAjouterReservationException("Impossible d'ajouter cette reservation");
 
         return new ResponseEntity<Reservation>(reservation, HttpStatus.CREATED);
@@ -50,4 +57,16 @@ public class ReservationController {
         Optional<Reservation> reservation = reservationRepository.findById(id.intValue());
         reservationRepository.deleteById(id.intValue());
     }
+
+    @PutMapping(value="/reservations")
+    public ResponseEntity<Reservation> updateReservation(@RequestBody Reservation reservation) {
+
+        reservationService.findById(reservation.getId());
+
+
+        Reservation updatedReservation = reservationService.updateReservation(reservation);
+
+        return ResponseEntity.ok().body(updatedReservation);
+    }
+
 }
