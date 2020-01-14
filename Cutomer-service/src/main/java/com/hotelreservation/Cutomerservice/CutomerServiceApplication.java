1156a95c829e4ebf392a191df5ed843c1d0710f5
syrine <syrinetajouri@gmail.com>
2020-01-14 14:56:57 +0100

    first commit

---
+++ Cutomer-service/src/main/java/com/hotelreservation/Cutomerservice/CutomerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.hotelreservation.Cutomerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CutomerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CutomerServiceApplication.class, args);
+	}
+
+}
---
+++ Cutomer-service/src/test/java/com/hotelreservation/Cutomerservice/CutomerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hotelreservation.Cutomerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CutomerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
