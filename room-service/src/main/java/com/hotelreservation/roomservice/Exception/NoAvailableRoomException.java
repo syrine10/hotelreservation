a6975006f8a5516d08d69ef91e0669323a84139f
syrine10 <syrinetajouri@gmail.com>
2020-01-14 22:19:17 +0100

    exception

---
+++ room-service/src/main/java/com/hotelreservation/roomservice/Exception/NoAvailableRoomException.java
@@ -0,0 +1,20 @@
+package com.hotelreservation.roomservice.Exception;
+
+public class NoAvailableRoomException extends Exception{
+
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
+
+    public NoAvailableRoomException() {
+        super("There is no room available");
+
+    }
+
+
+
+
+
+
+}
