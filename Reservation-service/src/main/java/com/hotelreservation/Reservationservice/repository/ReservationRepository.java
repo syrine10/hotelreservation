f55143d9fa1978aad8d10bf243c5e0a140cc2394
hamzafrigui <35801880+hamzafrigui@users.noreply.github.com>
2020-01-15 00:05:36 +0100

    Update ReservationRepository.java

--- Reservation-service/src/main/java/com/hotelreservation/Reservationservice/repository/ReservationRepository.java
+++ Reservation-service/src/main/java/com/hotelreservation/Reservationservice/repository/ReservationRepository.java
@@ -4,10 +4,14 @@ import com.hotelreservation.Reservationservice.model.Reservation;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-
-
+import java.util.Optional;
 
 
 @Repository
 public interface ReservationRepository extends JpaRepository<Reservation, Integer> {
+
+    void deleteById(Long id);
+
+
+    Optional<Reservation> findById(Long id);
 }
