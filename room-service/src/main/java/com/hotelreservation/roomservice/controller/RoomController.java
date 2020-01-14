9e41c1e98e6aea4614019d58e6208c4bef02ac87
syrine10 <syrinetajouri@gmail.com>
2020-01-14 20:25:55 +0100

    update controller

--- room-service/src/main/java/com/hotelreservation/roomservice/controller/RoomController.java
+++ room-service/src/main/java/com/hotelreservation/roomservice/controller/RoomController.java
@@ -1,4 +1,58 @@
 package com.hotelreservation.roomservice.controller;
 
+import com.hotelreservation.roomservice.Exception.NoAvailableRoomException;
+import com.hotelreservation.roomservice.model.Room;
+import com.hotelreservation.roomservice.repository.RoomRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+
+@RestController
 public class RoomController {
+    @Autowired
+    RoomRepository roomRepository;
+    @GetMapping(value="rooms/{id}")
+    ResponseEntity<Room> getProduct (@PathVariable Long  id) throws NoAvailableRoomException {
+
+
+        Room room = roomRepository.findById(id).orElseThrow(()-> new ResponseStatusException(HttpStatus.UNAUTHORIZED,"room not found = "+id));
+
+        return ResponseEntity.ok().body(room);
+
+    }
+
+
+
+    @GetMapping(value="rooms")
+    List<Room> getAllProducts () {
+
+        List<Room> room = roomRepository.findAll();
+        return room;
+
+
+    }
+
+    @PostMapping(value = "rooms")
+    ResponseEntity<Room> addNewRoom (@RequestBody Room room){
+
+        Room newRoom = roomRepository.save(room);
+        return ResponseEntity.ok().body(newRoom);
+    }
+
+    @DeleteMapping(value="rooms/{id}")
+    public void deleteRoom(@PathVariable Long id) throws  ResponseStatusException {
+
+        Room prod = roomRepository.findById(id).orElseThrow(()-> new ResponseStatusException(HttpStatus.UNAUTHORIZED,"room  "+id+" not found "));
+        roomRepository.deleteById(id);
+
+
+    }
+
+
+
 }
+
